1b7addbc2878f5c6e1b97478b929ab47823a56d2
MoyMFO <floresmoisesortiz@gmail.com>
2022-07-10 22:02:26 -0500

    Working on visualization of the OB

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ import numpy as np
 import pandas as pd
 import data as dt
 from functions import OrderBooksPreparation, XEMM
+from visualizations import XemmVisualization
+import warnings
+warnings.filterwarnings('ignore');
 
-ob_krak, ob_bit = dt.read_jsonOB(file_name = 'orderbooks_05jul21.json')
-obt = XEMM(ob_krak=ob_krak, ob_bit=ob_bit, bp=10)
+#ob_krak, ob_bit = dt.read_jsonOB(file_name = 'orderbooks_05jul21.json')
+#obt = XEMM(ob_krak=ob_krak, ob_bit=ob_bit, bp=10)
 
 #df_krak = ob_krak[list(ob_krak.keys())[0]]
 #df_bit = ob_bit[list(ob_bit.keys())[0]]
@@ -30,4 +33,6 @@ obt = XEMM(ob_krak=ob_krak, ob_bit=ob_bit, bp=10)
 #krak_bid = df_krak[['bid_size','bid']]
 #krak_ask = df_krak[['ask','ask_size']]
 
+plots = XemmVisualization()
+plots.orderbook_history()
 
--- visualizations.py
+++ visualizations.py
@@ -8,3 +8,30 @@
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+
+
+class XemmVisualization:
+
+    @staticmethod
+    def orderbook_history():
+        s = 365 * 15
+        df = pd.DataFrame({"date": pd.date_range("1-jan-2008", periods=s),
+                        "positive": np.random.uniform(0, 20, s),
+                        "negative": np.random.uniform(0, -5, s),})
+
+        fig = go.Figure([
+            go.Bar(x=df["date"], y=df["positive"], name="positive", marker_color="green"),
+            go.Bar(x=df["date"], y=df["negative"], name="negative", base=0, marker_color="red"),
+            ]).update_layout(
+            barmode="stack",
+            xaxis={
+                "range": [df.iloc[-200]["date"], df["date"].max()],
+                "rangeslider": {"visible": True},
+            },
+            margin={"l": 0, "r": 0, "t": 0, "r": 0},
+        )
+        return fig.show()
+        
